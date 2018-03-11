992450c09c1cedaa7055e4a3b7ef322c7e3ed8a5
“Cons <consbulaquena@gmail.com  git config --global user.name Cons Bulaquena git config --global user.email consbulaquena@gmail.com>
2018-03-12 00:36:15 +0800

    changed

--- GitXcodeTest/ViewController.swift
+++ GitXcodeTest/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         //just tryin
         //is it working?
+        // is it commiting
         
     }
 
